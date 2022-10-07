3cb1b584c7431e88de0853cf1ddfdccc5bec4b2d
aohanesian <103946712+aohanesian@users.noreply.github.com>
2022-10-08 02:31:57 +0300

    commit

--- hw21.js
+++ hw21.js
@@ -1,42 +1,28 @@
 `use strict`;
 
-const type = prompt(`exponentiation, multiplay, division, modulo?`);
-
-function exponentiation(a, b) {
-    let result = a ** b;
-    alert(result + ` exponentiation`);
+function mainFunction(callback) {
+    const a = +prompt(`Enter 1st number`);
+    const b = +prompt(`Enter 2nd number`);
+    return callback(a, b);
 }
 
-function multiplay(a, b) {
-    let result = a * b;
-    alert(result + ` multiplay`);
+function multiply(a, b) {
+    return alert(a * b);
+}
+
+function exponentiation(a, b) {
+    return alert(a ** b);
 }
 
 function division(a, b) {
-    let result = a / b;
-    alert(result + ` division`);
+    return alert(a / b);
 }
 
 function modulo(a, b) {
-    let result = a % b;
-    alert(result + ` modulo`);
+    return alert(a % b);
 }
 
-function mainFunction(type) {
-    const num1 = +prompt(`Enter 1st number`);
-    const num2 = +prompt(`Enter 2nd number`);
-
-    if (type === `exponentiation`) {
-        exponentiation(num1, num2);
-    } else if (type === `multiplay`) {
-        multiplay(num1, num2);
-    } else if (type === `division`) {
-        division(num1, num2);
-    } else if (type === `modulo`) {
-        modulo(num1, num2);
-    }
-}
-
-mainFunction(type, exponentiation, multiplay, division, modulo);
-
-
+mainFunction(multiply);
+mainFunction(exponentiation);
+mainFunction(division);
+mainFunction(modulo);
